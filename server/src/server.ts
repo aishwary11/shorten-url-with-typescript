8d344f2fccb3e08649688aca9e8ff99a24b012d4
aishwary11 <aishwary46@gmail.com>
2023-03-29 15:32:11 +0530

    some package removed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,16 @@
 import cors from 'cors';
 import 'dotenv/config';
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, { Express } from "express";
 import mongoose from 'mongoose';
 import { getUrls, postUrl, redirectUrl } from "./controllers/urlController";
 
 const app: Express = express();
 (() => mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/urlshortener").then(() => console.log('Connected to Mongo DB')).catch((err) => console.error(`Error in Mongo DB ${err}`)))();
 app.use(cors());
-app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log('[url path] :: ', req.path);
-    next();
-});
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     console.log('[url path] :: ', req.path);
+//     next();
+// });
 app.use(express.json());
 const port = process.env.PORT || 5000;
 app.get('/', getUrls);
