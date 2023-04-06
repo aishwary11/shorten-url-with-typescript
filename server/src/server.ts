8b3c16a724bc99fb18af0f706526444f1e918969
aishwary11 <aishwary46@gmail.com>
2023-04-06 18:35:35 +0530

    some more optimize

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,12 @@ import 'dotenv/config';
 import express, { Express, NextFunction, Request, Response } from "express";
 import mongoose from 'mongoose';
 import { getUrls, postUrl, reDirectUrl } from "./controllers/urlController";
+import { errorResp } from './utils/helpers';
 
 const app: Express = express();
 (() => mongoose.connect(process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/urlshortener").then(() => console.log('Connected to Mongo DB')).catch((err) => console.error(`Error in Mongo DB ${err}`)))();
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err) return res.status(500).json({ msg: `Error: ${err.message}` });
+app.use(async (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err) return await errorResp(res, 500, `Error: ${err.message}`);
     next();
 });
 app.use(cors());
