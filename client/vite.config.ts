ca38bc6dfe025d19f38fb00714808d61380c48b8
aishwary11 <aishwary46@gmail.com>
2023-04-09 15:51:40 +0530

    docker added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,10 +8,13 @@ export default defineConfig({
     outDir: "build",
     minify: true,
     sourcemap: false,
-    brotliSize: false,
     chunkSizeWarningLimit: 1048576
   },
   server: {
+    port: 3000,
+    cors: true
+  },
+  preview: {
     port: 3000,
     cors: true
   }
