bfcbc86f0578b0b8fa0569fe143adac3d5ef952f
aishwary11 <aishwary46@gmail.com>
2023-06-17 16:55:26 +0530

    auth middleware added

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { errorResp } from "../utils/helpers";
+
+type AuthenticatedRequest = Request & {
+  user?: any;
+};
+
+export const auth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return errorResp(res, 401, "Access Denied!");
+  const isTokenVerified = verifyToken(token, process.env.secret as string);
+  if (!isTokenVerified) return errorResp(res, 401, "Access Denied!");
+  else {
+    req.user = isTokenVerified;
+    next();
+  }
+};
+
+
+const verifyToken = (token: string, secret: string) => verify(token, secret);
