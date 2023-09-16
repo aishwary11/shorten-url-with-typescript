d2008bdb76044b891affa4fcc057ea6a3626b536
aishwary11 <aishwary46@gmail.com>
2023-09-17 00:23:00 +0530

    some more change

--- server/src/db/config.ts
+++ server/src/db/config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI as string);
+    const connect = await mongoose.connect(process.env.MONGODB_URI!);
     if (connect) console.log('Connected to MongoDB');
     else console.error("Error while connect", connect);
   } catch (error) {
