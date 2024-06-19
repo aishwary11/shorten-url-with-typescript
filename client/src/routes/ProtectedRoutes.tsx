6a768a898449b254997d724e214c7b403c680d9c
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-06-19 13:49:59 +0530

    some more change

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import constant from '../common/constant';
 
-const ProtectedRoutes: React.FC<{ children: React.ReactNode }> = ({ children }) => (localStorage.getItem(constant.token) ? <>{children}</> : <Navigate to="/login" />);
+const ProtectedRoutes: React.FC<{ children: React.ReactNode; }> = ({ children }) => {
+  const token = localStorage.getItem(constant.token);
+  return token ? children : <Navigate to="/login" />;
+};
 
 export default ProtectedRoutes;
