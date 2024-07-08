28ab17536622a5dcc0bbc94864883e8292eae376
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-07-08 12:02:56 +0530

    some more changes

--- server/src/common/utils/token.ts
+++ server/src/common/utils/token.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
 import constant from '../constant';
 const secret = constant.secret;
-export const generateToken = (payload: any) => jwt.sign(payload, secret, { expiresIn: constant.expire, algorithm: 'PS256' });
+export const generateToken = (payload: any) => jwt.sign(payload, secret, { expiresIn: constant.expire });
 export const verifyToken = (token: string) => jwt.verify(token, secret);
