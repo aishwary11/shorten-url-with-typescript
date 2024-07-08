f0b00c734aeb65f42a89ed7ac6049e18a8df0585
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-07-08 11:47:18 +0530

    some more changes

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import constant from '../common/constant';
 
-const ProtectedRoutes: React.FC<{ children: React.ReactNode; }> = ({ children }) => {
+const ProtectedRoutes: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const token = localStorage.getItem(constant.token);
   return token ? children : <Navigate to="/login" />;
 };
--- server/src/common/utils/responsehelpers.ts
+++ server/src/common/utils/responsehelpers.ts
@@ -1,5 +1,6 @@
 import { type Response } from 'express';
 
-export const responseHandler = (res: Response, statusCode: number = 200, msg: string = '', data?: any) => res.status(statusCode).json({ msg, data, status: statusCode < 400 });
+export default function responseHandler(res: Response, statusCode: number = 200, msg: string = '', data?: any) {
+  return res.status(statusCode).json({ msg, data, status: statusCode < 400 });
+}
 
-export default responseHandler;
