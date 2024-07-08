cacd2f0c0630cd91912e77f8288f60d7f6470c6f
Aishwary Shah <49820149+aishwary11@users.noreply.github.com>
2024-07-08 12:01:26 +0530

    some more changes

--- server/src/common/utils/responsehelpers.ts
+++ server/src/common/utils/responsehelpers.ts
@@ -3,4 +3,3 @@ import type { Response } from 'express';
 export default function responseHandler(res: Response, statusCode: number = 200, msg: string = '', data?: any) {
   return res.status(statusCode).json({ msg, data, status: statusCode < 400 });
 }
-
