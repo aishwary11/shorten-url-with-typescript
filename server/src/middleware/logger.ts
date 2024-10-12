375265804692ba4117ca0f5e8cf09575b728ab6b
aishwary11 <aishwary46@gmail.com>
2024-10-12 11:54:04 +0530

    some changes in logger

--- server/src/middleware/logger.ts
+++ server/src/middleware/logger.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import asyncHandler from '../common/utils/asynchandler';
 
-export default function logger(req: Request, res: Response, next: NextFunction) {
+const logger = asyncHandler(async (err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err) return next(err);
   console.log('Method:', req.method, 'Url: ', req.url, 'body: ', req.body, 'Time:', new Intl.DateTimeFormat().format(new Date()));
   next();
-}
+});
+
+export default logger;
