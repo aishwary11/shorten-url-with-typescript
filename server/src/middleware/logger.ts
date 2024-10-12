095f4f1db13b83969793a0e101c1198dc0bcfd0a
aishwary11 <aishwary46@gmail.com>
2024-10-12 11:55:52 +0530

    params added in logger

--- server/src/middleware/logger.ts
+++ server/src/middleware/logger.ts
@@ -3,7 +3,7 @@ import asyncHandler from '../common/utils/asynchandler';
 
 const logger = asyncHandler(async (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err) return next(err);
-  console.log('Method:', req.method, 'Url: ', req.url, 'body: ', req.body, 'Time:', new Intl.DateTimeFormat().format(new Date()));
+  console.log('Method:', req.method, 'Url: ', req.url, 'params: ' + JSON.stringify(req.params), 'body: ', req.body, 'Time:', new Intl.DateTimeFormat().format(new Date()));
   next();
 });
 
