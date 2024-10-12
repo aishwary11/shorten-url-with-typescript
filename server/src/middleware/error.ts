89685756b4bb2af96a6a293d9cc1e3184fa6457d
aishwary11 <aishwary46@gmail.com>
2024-10-12 11:35:26 +0530

    async handler added in error middleware

--- server/src/middleware/error.ts
+++ server/src/middleware/error.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import { STATUS_CODES } from '../common/constant';
+import asyncHandler from '../common/utils/asynchandler';
 import ErrorHandler from '../common/utils/errorhandler';
 import responseHandler from '../common/utils/responsehelpers';
 
-export default function errorMiddleware(err: ErrorHandler, req: Request, res: Response, next: NextFunction) {
+const errorMiddleware = asyncHandler(async (err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
   err.message ||= 'Internal Server Error';
   err.statusCode ||= STATUS_CODES.INTERNAL_SERVER_ERROR;
   return responseHandler(res, err.statusCode, err.message);
-}
+});
+export default errorMiddleware;
